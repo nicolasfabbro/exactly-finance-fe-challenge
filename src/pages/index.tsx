79013214e4d8ae9057d6b85d1e0fbaa12021c252
nfabbro-reciprocity <nicolas.fabbro@reciprocitylabs.com>
2022-05-31 23:28:56 +0200

    add eth and cdai balance2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useMetaMaskAccount } from '../providers/MetaMaskProvider';
 import { ProtectedPageLayout } from '../components/layouts/ProtectedPageLayout';
 import type { NextPage } from 'next';
@@ -5,13 +6,16 @@ import type { NextPage } from 'next';
 import styles from '../styles/Home.module.scss';
 
 const Home: NextPage = () => {
-  const { isConnected, connectedAccount } = useMetaMaskAccount();
+  const { isConnected, connectedAccount, accountBalance } = useMetaMaskAccount();
 
   return (
     <ProtectedPageLayout canAccess={isConnected}>
       <h1>Exactly finance</h1>
       ------------------------
-      <p>Connected account: {connectedAccount}</p>
+      <h3>Connected account: {connectedAccount}</h3>
+      <h6>Balance: ETH {accountBalance?.eth}</h6>
+      <h6>Balance: cDAI {accountBalance?.cdai}</h6>
+      <h6>Balance: DAI {accountBalance?.dai}</h6>
     </ProtectedPageLayout>
   )
 }
--- src/providers/MetaMaskProvider.tsx
+++ src/providers/MetaMaskProvider.tsx
@@ -1,11 +1,18 @@
 import { useState, useEffect, createContext, useContext, FC, ReactNode } from "react";
 import { MetaMaskInpageProvider } from "@metamask/providers";
+import { ethers } from 'ethers';
+import cDAITokenAbi from '../utils/cDAITokenAbi.json';
 
 type MetaMaskContextTypes = {
-  ethereum: MetaMaskInpageProvider | null;
+  ethereum: any;
   isConnected: boolean;
   connectedAccount: string | undefined;
   connectAccount: () => void;
+  accountBalance: {
+    eth?: string;
+    cdai?: any;
+    dai?: any;
+  };
 };
 
 const MetaMaskAccountContext = createContext<MetaMaskContextTypes>({
@@ -13,6 +20,7 @@ const MetaMaskAccountContext = createContext<MetaMaskContextTypes>({
   isConnected: false,
   connectedAccount: '',
   connectAccount: () => { },
+  accountBalance: { },
 });
 
 type ProviderProps = {
@@ -20,11 +28,15 @@ type ProviderProps = {
 };
 
 const MetaMaskAccountProvider: FC<ProviderProps> = ({ children }) => {
-  const [ethereum, setEthereum] = useState<MetaMaskInpageProvider | null>(null);
+  const cDAIAddress = '0xF0d0EB522cfa50B716B3b1604C4F0fA6f04376AD';
+  const DAIAddress = '0x468409860F155Ec6A90ba64ecBa077eaA6C1f5A5';
+
+  const [ethereum, setEthereum] = useState<any>(null);
   const [connectedAccount, setConnectedAccount] = useState<string | undefined>('');
+  const [balance, setBalance] = useState<MetaMaskContextTypes['accountBalance']>({});
 
   const setEthereumFromWindow = async () => {
-    if(window.ethereum) {
+    if (window.ethereum) {
       // Reload if chain changes
       window.ethereum.on('chainChanged', () => window.location.reload());
 
@@ -33,7 +45,7 @@ const MetaMaskAccountProvider: FC<ProviderProps> = ({ children }) => {
 
       const chainId = await window.ethereum.request({ method: 'eth_chainId' });
       const kovanId = '0x2a'; // See <https://docs.metamask.io/guide/ethereum-provider.html#chain-ids>
-      if(chainId === kovanId) {
+      if (chainId === kovanId) {
         setEthereum(window.ethereum);
       } else {
         alert('Please use Kovan network');
@@ -44,7 +56,7 @@ const MetaMaskAccountProvider: FC<ProviderProps> = ({ children }) => {
   const handleAccounts = (accounts: string[]) => {
     if (accounts.length > 0) {
       const account = accounts[0];
-      console.log('We have an authorized account: ', account);
+      // console.log('We have an authorized account: ', account);
       setConnectedAccount(account);
     } else {
       console.log("No authorized accounts yet")
@@ -54,8 +66,8 @@ const MetaMaskAccountProvider: FC<ProviderProps> = ({ children }) => {
   const getConnectedAccount = async () => {
     if (ethereum) {
       const accounts = await ethereum.request({ method: 'eth_accounts' });
-      console.log('getConnectedAccount - eth_accounts', accounts);
-      
+      // console.log('getConnectedAccount - eth_accounts', accounts);
+
       if (accounts && Array.isArray(accounts)) {
         handleAccounts(accounts);
       }
@@ -75,20 +87,45 @@ const MetaMaskAccountProvider: FC<ProviderProps> = ({ children }) => {
     }
   };
 
-  
+  const getAccountBalance = async () => {
+    if (ethereum && connectedAccount) {
+      const provider = new ethers.providers.Web3Provider(ethereum, "any");
+
+      const cDAIContract = new ethers.Contract(cDAIAddress, cDAITokenAbi, provider);
+      const cDAIBalance = await cDAIContract.balanceOf(connectedAccount);
+
+      // const DAIContract = new ethers.Contract(DAIAddress, cDAITokenAbi, provider);
+      // const DAIBalance = await DAIContract.balanceOf(connectedAccount);
+
+      const ethBalance = await provider.getBalance(connectedAccount);
+
+      return {
+        eth: ethers.utils.formatEther(ethBalance),
+        cdai: cDAIBalance.toString(),
+        // dai: DAIBalance.toString(),
+      }
+    }
+  };
+
+
   useEffect(() => {
     setEthereumFromWindow();
   }, []);
 
   useEffect(() => {
-    getConnectedAccount()
-  });
+    getConnectedAccount();
+
+    getAccountBalance().then(res => {
+      setBalance(res || {});
+    })
+  }, [ethereum, connectedAccount]);
 
   const value = {
     ethereum,
     isConnected: Boolean(connectedAccount),
     connectedAccount,
-    connectAccount
+    connectAccount,
+    accountBalance: balance,
   };
 
   return (
